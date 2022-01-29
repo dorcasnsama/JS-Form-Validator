bd8d69b3561f293c8090859dcdee6e561bbba398
dorcasnsama <91195819+dorcasnsama@users.noreply.github.com>
2022-01-29 23:58:58 +0200

    Update script.js
    Corrected the js to make the website responsive

--- form validator/script.js
+++ form validator/script.js
@@ -13,10 +13,10 @@ form.addEventListener('submit' , (e) => {
 
 function checkInputs() {
     //get values for the inputs
-    const usernameValue = username.Value.trim();
-    const emailValue = email.Value.trim();
-    const passwordValue = password.Value.trim();
-    const password2Value = password2.nodeValue.trim();
+    const usernameValue = username.value.trim();
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+    const password2Value = password2.value.trim();
 
     if(usernameValue === ''){
         //show error
